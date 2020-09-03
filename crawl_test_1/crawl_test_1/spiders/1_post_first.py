0388a47f38ca2597dd79f3740cc5023730c06e73
Sjing2 <sojeong31@hanmail.net>
2020-09-03 20:05:39 +0900

    수정

--- crawl_test_1/crawl_test_1/spiders/1_post_first.py
+++ crawl_test_1/crawl_test_1/spiders/1_post_first.py
@@ -14,8 +14,8 @@ class Post_Spider(scrapy.Spider):
 
     def __init__(self):
         try:
-            # id_file = input("id 파일명(경로 포함)을 입력해 주세요(ex. D:\Scrapy\insta_crawling\insta_crawling\jl_1.csv)\n")
-            self.df = pd.read_csv('/home/lab19/sjing_e/SJ_practice/crawl_test_1/crawl_test_1/cc_word.csv')
+            id_file = input("id 파일명(경로 포함)을 입력해 주세요(ex. D:\Scrapy\insta_crawling\insta_crawling\jl_1.csv)\n")
+            self.df = pd.read_csv(r"{}".format(id_file))
             self.inner_id_ls = list(map(str, list(np.unique(self.df['inner_id']))))
         except:
             print("id_file_error!")
